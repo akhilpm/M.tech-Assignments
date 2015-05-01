8d0efec22a5c2f3ae0d36d7d4cf15e5f497b57f8
Akhil P M <akhilpm135@gmail.com>
2015-05-01 22:49:21 +0530

    Update routing.py

--- ReinforcementLearning/Qrouting/routing.py
+++ ReinforcementLearning/Qrouting/routing.py
@@ -13,11 +13,11 @@ Qmatrix=np.zeros(shape=(3,4))
 grid structure is as shown below
 
 		1----2     3----4
-		|				|
-		|				|
+		|		|
+		|		|
 		5----6-----7----8
-		|				|
-		|				|
+		|		|
+		|		|
 		9---10----11---12
 
 '''
